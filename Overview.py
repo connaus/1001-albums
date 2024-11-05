576284179e65c64bf3a4a9863b657f3d0576385b
connaus <connaus@tcd.ie>
2024-11-05 12:03:59 +0000

    tidying overview code

--- Overview.py
+++ Overview.py
@@ -1,10 +1,4 @@
-from pathlib import Path
 import streamlit as st
-
-from cfg.cfg import load_config
-from src.album import Album, load_albums
-
-SETTING_PATH = Path("C:/Users/ste-c/OneDrive/Documents/1001-albums/cfg/setting.yaml")
 
 
 def main() -> None:
