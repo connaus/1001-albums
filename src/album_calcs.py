e99e80407ba51b16fe72d2493aa02f94e6514b83
connaus <connaus@tcd.ie>
2024-11-08 12:08:39 +0000

    adding overview calcs

--- src/album_calcs.py
+++ src/album_calcs.py
@@ -4,6 +4,17 @@ from datetime import timedelta
 from src.album import Album
 
 
+def latest_album_key(albums: list[Album]) -> int:
+    return min([album.album_number for album in albums if album.listened == False])
+
+
+def albums_listened_to(albums: list[Album]) -> int:
+    return len([album for album in albums if album.listened == True])
+
+
+def albums_previously_listened_to(albums: list[Album]) -> int:
+    return len([album for album in albums if album.previous_listened == True])
+
 
 def total_albums_by_year(albums: list[Album]) -> dict[int, int]:
     d = defaultdict(int)
