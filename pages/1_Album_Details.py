3425908be61a93cef878bb0a76e55dea5f42ccc5
connaus <connaus@tcd.ie>
2024-12-23 15:11:46 +0000

    save buttons activate placeholder save code

--- pages/1_Album_Details.py
+++ pages/1_Album_Details.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 import streamlit as st
 
-from cfg.cfg import Config, load_config
+from cfg.cfg import Config
 from src.album import Album
 
 
@@ -28,16 +28,72 @@ def save_album_details(cfg: Config) -> None:
         )
 
 
-album = update_album_key(0)
-left, _, right = st.columns(3)
-if left.button("Previous", use_container_width=True):
-    album = update_album_key(-1)
-if right.button("Next", use_container_width=True):
-    album = update_album_key(1)
-st.markdown(f"# Album {album.album_number}")
-st.markdown(f"# {album.album_title}")
-st.markdown(f"{album.artist}")
-st.markdown(f"{album.total_time}")
+class PageState:
 
-st.markdown("# Comment")
-st.markdown(f"{album.comments}")
+    DEFAULT = "default"
+    EDIT = "edit"
+    SAVE = "save"
+
+
+if "page_state" not in st.session_state:
+    st.session_state.page_state = PageState.DEFAULT
+
+album = update_album_key(0)
+
+left, middle, right = st.columns(3)
+if middle.button(
+    "Edit",
+    use_container_width=True,
+):
+    if st.session_state.page_state == PageState.DEFAULT:
+        st.session_state.page_state = PageState.EDIT
+    elif st.session_state.page_state == PageState.EDIT:
+        st.session_state.page_state = PageState.SAVE
+if left.button(
+    "Previous",
+    use_container_width=True,
+    disabled=(
+        False
+        if st.session_state.page_state in (PageState.DEFAULT, PageState.SAVE)
+        else True
+    ),
+):
+    album = update_album_key(-1)
+
+if right.button(
+    "Next",
+    use_container_width=True,
+    disabled=(
+        False
+        if st.session_state.page_state in (PageState.DEFAULT, PageState.SAVE)
+        else True
+    ),
+):
+    album = update_album_key(1)
+
+if st.session_state.page_state == PageState.EDIT:
+    st.markdown(f"# Album Number")
+    album_number = st.text_area(
+        " ", f"{album.album_number}", label_visibility="collapsed"
+    )
+    st.markdown(f"# {album.album_title}")
+    st.markdown(f"{album.artist}")
+    st.markdown(f"{album.total_time}")
+
+    st.markdown("# Comment")
+    comment = st.text_area(" ", f"{album.comments}", label_visibility="collapsed")
+    st.button("Save")
+    st.session_state.page_state = PageState.SAVE
+
+elif st.session_state.page_state == PageState.SAVE:
+    print("Saving Data")
+    st.session_state.page_state = PageState.DEFAULT
+
+if st.session_state.page_state == PageState.DEFAULT:
+    st.markdown(f"# Album {album.album_number}")
+    st.markdown(f"# {album.album_title}")
+    st.markdown(f"{album.artist}")
+    st.markdown(f"{album.total_time}")
+
+    st.markdown("# Comment")
+    st.markdown(f"{album.comments}")
