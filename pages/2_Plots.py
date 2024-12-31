8b6e7683ae1ee0fe2e7a6fb5fbca4792800be031
connaus <connaus@tcd.ie>
2024-12-31 16:58:59 +0000

    moving to plotly plots

--- pages/2_Plots.py
+++ pages/2_Plots.py
@@ -1,32 +1,29 @@
-from src.album import Album
-
-# import matplotlib.figure as fig
-import matplotlib.pyplot as plt
-import matplotlib.ticker as tkr
 import src.album_calcs as ac
 import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
 
+fig = px.bar(
+    ac.album_listened_status_by_year(),
+    x="Year",
+    y="Albums",
+    color="Status",
+    barmode="stack",
+    title="Album Status by Year",
+)
+# fig.show()
+st.plotly_chart(fig)
 
-albums: list[Album] = st.session_state.albums
-total = ac.total_albums_by_year(albums)
-listend = ac.listened_albums_by_year(albums)
-listend_time = ac.listened_time_by_year(albums)
-
-fig, axs = plt.subplots(1, 2)
-ax1, ax2 = axs
-
-labels = ("Total Albums", "Listened Albums")
-ax1.bar(x=list(total.keys()), height=list(total.values()))
-ax1.plot(list(listend.keys()), list(listend.values()), "ro-", markersize=3)
-ax1.set_title("Albums per Year")
-
-
-def format_seconds(seconds: float, _) -> str:
-    return f"{int(seconds // 3600)}"
-
-
-ax2.plot(list(listend_time.keys()), list(listend_time.values()), "o-", markersize=3)
-ax2.yaxis.set_major_formatter(format_seconds)
-ax2.set_title("Time Listened")
-ax2.yaxis.set_major_locator(tkr.MultipleLocator(base=3600))
-st.write(fig)
+df = ac.time_listened_by_year()
+fig = px.line(
+    df,
+    x="Year",
+    y=df["Time"] + pd.to_datetime("1970/01/01"),
+    title="Time Listened by Year",
+    markers=True,
+)
+figure = go.Figure(data=fig)
+figure.update_layout(yaxis_tickformat="%H:%M.%f")
+# fig.show()
+st.plotly_chart(fig)
--- src/album_calcs.py
+++ src/album_calcs.py
@@ -1,7 +1,8 @@
 from collections import defaultdict
 from datetime import timedelta
-
+import pandas as pd
 from src.album import Album
+import streamlit as st
 
 
 def next_album(albums: list[Album]) -> Album:
@@ -56,10 +57,46 @@ def total_listened_time(albums: list[Album]) -> timedelta:
 
 
 def total_albums_by_year(albums: list[Album]) -> dict[int, int]:
+    """returns a dictionary of the form {year: number of albums}"""
     d = defaultdict(int)
     for album in albums:
         d[album.release_date] += 1
     return d
+
+
+def album_listened_status_by_year() -> pd.DataFrame:
+    """returns a dataframe of the form {Year, Status, Albums}. Status is one of "Previously Heard", "Listened", "Unlistened"
+    the year is unique"""
+    d = defaultdict(list)
+    for album in st.session_state.albums:
+        d["Year"].append(album.release_date)
+        if album.previous_listened:
+            status = "Previously Heard"
+        elif album.listened:
+            status = "Listened"
+        else:
+            status = "Unlistened"
+        d["Status"].append(status)
+        d["Albums"].append(1)
+
+    listend_df = pd.DataFrame(d)
+    listend_df = listend_df.groupby(["Year", "Status"]).sum().reset_index()
+    return listend_df
+
+
+def time_listened_by_year() -> pd.DataFrame:
+    """returns a dataframe of the form {Year, Status, Albums}. Status is one of "Previously Heard", "Listened", "Unlistened"
+    the year is unique"""
+    d = defaultdict(list)
+    for album in st.session_state.albums:
+        if not album.listened:
+            continue
+        d["Year"].append(album.release_date)
+        d["Time"].append(album.total_time)
+
+    listend_df = pd.DataFrame(d)
+    listend_df = listend_df.groupby(["Year"]).sum().reset_index()
+    return listend_df
 
 
 def listened_albums_by_year(albums: list[Album]) -> dict[int, int]:
